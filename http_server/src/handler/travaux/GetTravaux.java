71d2fd39b7a79951b09c2486272c68d6f9625397
Mape57 <matheo.pedron57@gmail.com>
2024-06-13 11:23:20 +0200

    + récupération des incidents avec le RMI + ajout de la direction /travaux

---
+++ http_server/src/handler/travaux/GetTravaux.java
@@ -0,0 +1,16 @@
+package handler.travaux;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import serveur.ServeurCentral;
+import tools.ExchangeContentSender;
+
+import java.io.IOException;
+
+public class GetTravaux implements HttpHandler {
+	@Override
+	public void handle(HttpExchange exchange) throws IOException {
+		String content = ServeurCentral.incident.getData();
+		ExchangeContentSender.send(exchange, content);
+	}
+}
--- http_server/src/rmi/RestaurantDataRequesterInterface.java
+++ http_server/src/rmi/RestaurantDataRequesterInterface.java
@@ -3,7 +3,7 @@ package rmi;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface DataRequester extends Remote {
+public interface RestaurantDataRequesterInterface extends Remote {
 	String getAllRestaurantPosition() throws RemoteException;
 	String getRestaurantById(int idResto) throws RemoteException;
 }
