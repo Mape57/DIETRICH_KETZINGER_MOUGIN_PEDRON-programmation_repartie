1b39d0a42d2010668a234369f9edbeaad7003dbd
Tomm <tom.ketzinger6@etu.univ-lorraine.fr>
2024-06-17 14:08:02 +0200

    ajout photo

---
+++ web_client/recup_restaurants.js
@@ -0,0 +1,53 @@
+const UNSPLASH_ACCESS_KEY = 'demande a ta mere'; // Remplacez par votre clé API
+let imageIndex = 0; // Compteur global pour suivre l'index des images
+
+// Récupère la liste des restaurants avec leurs coordonnées
+export function fetchRestaurantList() {
+	return fetch("http://localhost:8080/restaurants")
+		.then(response => response.json())
+		.then(data => {
+			return data.map(station => {
+				const [lat, lon] = station.coordonnees.split(',').map(Number);
+				return { idResto: station.idResto, lat, lon };
+			});
+		})
+		.catch(error => {
+			console.error('Erreur lors de la récupération de la liste des restaurants:', error);
+			return [];
+		});
+}
+
+// Récupère les détails d'un restaurant par son ID et ajoute une image de Unsplash
+export async function fetchRestaurantDetails(idResto) {
+	return fetch(`http://localhost:8080/restaurants/${idResto}`)
+		.then(response => response.json())
+		.then(async data => {
+			const [lat, lon] = data.coordonnees.split(',').map(Number);
+			const imageUrl = await fetchRestaurantImage(data.nomResto);
+			return {
+				...data,
+				lat,
+				lon,
+				imageUrl
+			};
+		})
+		.catch(error => {
+			console.error(`Erreur lors de la récupération des détails du restaurant ${idResto}:`, error);
+			return null;
+		});
+}
+
+// Fonction pour obtenir une image de Unsplash pour un restaurant
+async function fetchRestaurantImage(restaurantName) {
+	console.log(restaurantName)
+	const response = await fetch(`https://api.unsplash.com/search/photos?query=restaurant,${encodeURIComponent(restaurantName)}&client_id=${UNSPLASH_ACCESS_KEY}`);
+	const data = await response.json();
+	if (data.results && data.results.length > 0) {
+		const imageUrl = data.results[imageIndex % data.results.length].urls.small; // Utiliser l'image à l'index actuel
+		imageIndex++; // Incrémenter l'index pour le prochain restaurant
+		return imageUrl;
+	} else {
+		// URL d'une image par défaut si aucune image n'est trouvée
+		return 'https://via.placeholder.com/250?text=Restaurant+Image';
+	}
+}
