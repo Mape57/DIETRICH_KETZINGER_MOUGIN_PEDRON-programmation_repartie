f4b9f3795a14f9dace6ba159afd2ee06043b838c
Mape57 <matheo.pedron57@gmail.com>
2024-06-18 13:49:44 +0200

    + correction erreur CORS

--- http_server/src/handler/restaurant/RestaurantsHandler.java
+++ http_server/src/handler/restaurant/RestaurantsHandler.java
@@ -12,27 +12,41 @@ import java.rmi.RemoteException;
 public class RestaurantsHandler implements HttpHandler {
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
-		String content = "";
-
-		if (exchange.getRequestMethod().equals("GET")) {
-			content = ServeurCentral.restaurant.getAllRestaurantPosition();
+		if (exchange.getRequestMethod().equalsIgnoreCase("OPTIONS")) {
+			exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+			exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, OPTIONS, POST");
+			exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type, Authorization");
+			exchange.sendResponseHeaders(204, -1);
+		} else if (exchange.getRequestMethod().equals("GET")) {
+			String content = ServeurCentral.restaurant.getAllRestaurantPosition();
+			ExchangeContentSender.send(exchange, content, 200);
 		} else if (exchange.getRequestMethod().equals("POST")) {
+			System.out.println("here");
 			byte[] body = exchange.getRequestBody().readAllBytes();
-			content = postRestaurant(new String(body));
+			System.out.println("before");
+			String content = postRestaurant(new String(body));
+			System.out.println(content);
+			ExchangeContentSender.send(exchange, content, 200);
 		} else {
 			ExchangeContentSender.send(exchange, "Les paramètres fournis ne sont pas corrects.", 400);
 		}
-
-		ExchangeContentSender.send(exchange, content, 200);
 	}
 
 	private String postRestaurant(String body) throws RemoteException {
+		System.out.println(body);
 		JSONObject json = new JSONObject(body);
+		System.out.println("ok");
 
 		String nomResto = json.getString("nomResto");
 		String adr = json.getString("adr");
 		String coordonnees = json.getString("coordonnees");
 		int note = json.getInt("note");
+
+		System.out.println("=====================================");
+		System.out.println(nomResto);
+		System.out.println(adr);
+		System.out.println(coordonnees);
+		System.out.println(note);
 
 		return ServeurCentral.restaurant.postRestaurant(nomResto, adr, coordonnees, note);
 	}
