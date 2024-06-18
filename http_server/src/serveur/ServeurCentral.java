553ff4ddfd816be8ec1f5615a68665798c3d4c5a
Mape57 <matheo.pedron57@gmail.com>
2024-06-18 09:44:26 +0200

    + tentative de passage en https

--- http_server/src/serveur/ServeurCentral.java
+++ http_server/src/serveur/ServeurCentral.java
@@ -1,5 +1,10 @@
 package serveur;
 
+import com.sun.net.httpserver.HttpsServer;
+import com.sun.net.httpserver.HttpsConfigurator;
+import com.sun.net.httpserver.HttpsParameters;
+import javax.net.ssl.*;
+
 import com.sun.net.httpserver.HttpServer;
 import handler.restaurant.ReservationHandler;
 import handler.restaurant.RestaurantByIdHandler;
@@ -7,12 +12,15 @@ import handler.restaurant.RestaurantsHandler;
 import handler.travaux.TravauxHandler;
 import rmi.*;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.rmi.AlreadyBoundException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.security.KeyStore;
+import java.util.Objects;
 
 public class ServeurCentral {
 	public static RestaurantDataRequesterInterface restaurant;
@@ -27,8 +35,8 @@ public class ServeurCentral {
 		rebindRestaurant(registry);
 		rebindTravaux(registry);
 
-		System.out.println("Création du serveur HTTP");
-		HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+		HttpsServer server = HttpsServer.create(new InetSocketAddress(8080), 0);
+		server.setHttpsConfigurator(new HttpsConfigurator(Objects.requireNonNull(createSSLContext())));
 		server.createContext("/restaurants/", new RestaurantByIdHandler());
 		server.createContext("/restaurants", new RestaurantsHandler());
 		server.createContext("/reservation", new ReservationHandler());
@@ -60,5 +68,31 @@ public class ServeurCentral {
 		} catch (Exception e) {
 			System.out.println("Erreur lors du rebind du registre travaux");
 		}
+	}
+
+	private static SSLContext createSSLContext() {
+		try {
+			// Generate a keystore, replace "password" and "myKeyStore.jks" with your actual password and keystore file
+			char[] password = "password".toCharArray();
+			KeyStore ks = KeyStore.getInstance("JKS");
+			FileInputStream fis = new FileInputStream("keystore.jks");
+			ks.load(fis, password);
+
+			// Set up the key manager factory
+			KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+			kmf.init(ks, password);
+
+			// Set up the trust manager factory
+			TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+			tmf.init(ks);
+
+			// Set up the HTTPS context and parameters
+			SSLContext sslContext = SSLContext.getInstance("TLS");
+			sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+			return sslContext;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
